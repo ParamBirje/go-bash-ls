bb0c0e259b76fe73e2a19368110772e06bfe32db
Param Birje <parambirje@gmail.com>
2024-06-19 14:22:54 +0530

    Now able to get children of cwd

--- main.go
+++ main.go
@@ -11,5 +11,22 @@ func main() {
 		panic("Couldn't get the current working directory.")
 	}
 
-	fmt.Println(cwd)
+	fmt.Println("CWD:", cwd)
+
+	entries, err := os.ReadDir(cwd)
+	if err != nil {
+		panic("Couldn't read directory.")
+	}
+
+	var directories []string
+	for _, entry := range entries {
+		// if entry.IsDir() {
+		directories = append(directories, entry.Name())
+		// }
+	}
+
+	fmt.Println("\nChildren:")
+	for _, dir := range directories {
+		fmt.Println(dir)
+	}
 }
